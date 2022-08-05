791b84ac2dcd798aeb5dbcfdcc24f3f1299685f6
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-08-05 14:09:57 -0600

    Change header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = () => {
   return (
     <header>
-      <h1 className='title'>☆・゜Rocky Mountain VibeCheck ゜・☆</h1>
+      <h1 className='title'>☆・゜Rocky Mountain Vibes ゜・☆</h1>
       <p className='line'>*・゜・*:.。.*.。.:*・ç・*:.。.*.。.:*・☆・゜・*:.。.*.。.:*・☆・゜・*:.。.:*・☆・゜・*:.。.*.。.:*・゜・*</p>
     </header>
   )
