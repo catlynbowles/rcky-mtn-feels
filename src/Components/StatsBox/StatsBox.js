520960604417ec205238eb10123d77c2aaecc85e
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-08-05 15:04:26 -0600

    Add secondary emotions feature"

--- src/Components/StatsBox/StatsBox.js
+++ src/Components/StatsBox/StatsBox.js
@@ -2,11 +2,25 @@ import React from 'react'
 import './StatsBox.css'
 import PropTypes from 'prop-types'
 
-const StatsBox = ({localTotals, globalTotals}) => {
+const StatsBox = ({localTotals, globalTotals, secondaryEmotions}) => {
+
+  const secondStyle = {
+    'color': '#0922e4',
+    'text-shadow': '0 0 4px #0922e4'
+  }
+
+  const generateSecondaryEmotions = secondaryEmotions.length > 0 && secondaryEmotions.map(emotion => {
+      console.log(emotion)
+      return (
+        <p className='small-header}' style={secondStyle}>{emotion.name}</p>
+      )
+    })
+  
   return (
     <div>
       <p className='totals'> {localTotals.toLocaleString()} others in your region.</p>
       <p className='totals'>{globalTotals.toLocaleString()} in the world.</p>
+      {generateSecondaryEmotions.length > 1 && <div className='small-header'>You might also relate to: {generateSecondaryEmotions.slice(0, 3)}</div>}
     </div>
   )
 }
