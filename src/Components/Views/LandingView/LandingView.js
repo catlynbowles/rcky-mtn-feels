7568c673b237113b47284db3cc80a4ddce773e3c
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-09-07 19:12:53 -0600

    Add rough styling and info to about page"

--- src/Components/Views/LandingView/LandingView.js
+++ src/Components/Views/LandingView/LandingView.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import './LandingView.scss'
 
 const LandingView = () => {
   return (
     <section className='about-section'>
-      <h1>About Rocky Mountain Vibes</h1>
+      <h1 className='about-title'>About Rocky Mountain Vibes</h1>
+      <p className='about-description'>&emsp; The purpose of this project is to create a tool that helps us celebrate our own emotional capacity with others. At times, it’s too easy to feel intense emotions in isolation, promoting the false association that we are alone in feeling. The truth, however, is that we are never alone. This app helps the user build that connection. <br></br><br></br>
+
+      &emsp; The app includes several features. The ‘My Diary’ tab allows a user to capture and save an emotional feeling. The feeling remains captured until the ‘reset’ button is clicked. The ‘Connect Me’ feature allows the user to take their own emotional temperature, choosing one of the primary emotions. The user will then be navigated to a page which displays others in the MST time zone reporting feeling this same way, and also calculates the global total.<br></br><br></br>
+
+      &emsp; This app was created using React and was refactored to include the local storage feature in ‘My Diary’. As a frontend software engineer, I hope this will be one of many projects I work on that uses technology to help us connect to one another.
+      </p>
     </section>
   )
 }
