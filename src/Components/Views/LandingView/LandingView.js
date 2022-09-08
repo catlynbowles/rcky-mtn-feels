093c138d40c83745e7091452eb22084041951c55
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-09-08 11:06:08 -0600

    Add landing page pngs "

--- src/Components/Views/LandingView/LandingView.js
+++ src/Components/Views/LandingView/LandingView.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import './LandingView.scss'
 import catlynPhoto from '../../../assets/CatlynB.jpeg';
+import tentPhoto from '../../../assets/7.png';
+import mtnPhoto from '../../../assets/9.png';
 
 const LandingView = () => {
   return (
     <section className='about-section'>
       <h1 className='about-title'>About Rocky Mountain Vibes</h1>
-      <p className='about-description'>&emsp; The purpose of this project is to create a tool that helps us celebrate our own emotional capacity with others. At times, it’s too easy to feel intense emotions in isolation, promoting the false association that we are alone in feeling. The truth, however, is that we are never alone. This app helps the user build that connection. <br></br><br></br>
-
-      &emsp; The app includes several features. The ‘My Diary’ tab allows a user to capture and save an emotional feeling. The feeling remains captured until the ‘reset’ button is clicked. The ‘Connect Me’ feature allows the user to take their own emotional temperature, choosing one of the primary emotions. The user will then be navigated to a page which displays others in the MST time zone reporting feeling this same way, and also calculates the global total.<br></br><br></br>
-
-      &emsp; This app was created using React and was refactored to include the local storage feature in ‘My Diary’. It relies heavily on the We Feel API, which calculates the totals displayed from tweets occuring throughout the world. As a frontend software engineer, I hope this will be one of many projects I work on that uses technology to help us connect to one another.
-      </p>
-      <div className="photo-container">
-          <img
-            data-cy="catlyn"
-            src={catlynPhoto}
-            alt="headshot of Catlyn"
-          ></img>
-          <h3>Catlyn Bowles is a lifelong resident of Colorado.</h3>
-            <a href="https://www.linkedin.com/in/catlyn-bowles/">Learn More</a>
+      <div className='photo-text-container'>
+        <img src={tentPhoto} height='500' />
+        <div className='text-container'>
+          <h3>The Mission</h3>
+          <p className='about-description'>
+            &emsp; The purpose of this project is to create a tool that helps us
+            celebrate our own emotional capacity with others.
+            At times, it’s too easy to feel intense emotions in isolation,
+            promoting the false association that we are alone in feeling.
+            The truth, however, is that we are never alone. This app helps the user build that connection.
+            This app was created using React and was refactored to include the local storage feature in ‘My Diary’.
+            It relies heavily on the We Feel API, which calculates the totals displayed from tweets occuring throughout the world.
+            As a frontend software engineer, I hope this will be one of many projects I work on that uses technology to help us connect to one another.
+          </p>
         </div>
+      </div>
+      <div className='photo-text-container'>
+        <div className='text-container'>
+          <h3>Features</h3>
+          <p className='about-description'>&emsp; The app includes several features.
+            The ‘My Diary’ tab allows a user to capture and save an emotional feeling.
+            The feeling remains captured until the ‘reset’ button is clicked.
+            The ‘Connect Me’ feature allows the user to take their own emotional temperature,
+            choosing one of the primary emotions. The user will then be navigated to a page
+            which displays others in the MST time zone reporting feeling this same way,
+            and also calculates the global total.</p>
+        </div>
+        <img src={mtnPhoto} height='500' />
+      </div>
+      <div className="photo-container">
+        <img
+          data-cy="catlyn"
+          src={catlynPhoto}
+          alt="headshot of Catlyn"
+        ></img>
+        <a href="https://www.linkedin.com/in/catlyn-bowles/">Connect with me!</a>
+      </div>
     </section>
   )
 }
