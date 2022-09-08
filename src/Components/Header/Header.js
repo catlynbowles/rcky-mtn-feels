2408b669022c54cde2fdeade800cfd156d921a97
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-09-08 11:54:16 -0600

    Add correct pngs to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import './Header.scss'
 import Nav from '../Nav/Nav'
-import tree from '../../assets/23.png'
+import backpack from '../../assets/20.png'
+import lantern from '../../assets/23.png'
 
 const Header = () => {
   return (
     <header className='header'>
       <div className='together'>
-        <img src={tree} height='100' color='-1' />
+        <img src={lantern} height='100' color='-1' />
         <h1 className='title'>Rocky Mountain Vibes</h1>
-        <img src={tree} height='100' color='-1' />
+        <img src={backpack} height='100' color='-1' />
       </div>
       <Nav />
     </header>
