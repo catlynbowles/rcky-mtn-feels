225784bf8eb6d8f31788ed61ea9c69d7f662285e
Catlyn Bowles <bowlescatlyn@gmail.com>
2023-05-06 16:52:53 -0600

    adjust font size for media breakpoints on header text

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <header>
-      <h1 className="title">☆・゜Vibe Check ゜・☆</h1>
+      <h1 className="title">・☆・Vibe Check・☆・</h1>
       <span className="line">
         <span className="end">*・゜・*:.。.*.。.:*・ç・*:.。.*.。.:*・☆・</span>
         <span className="middle">
